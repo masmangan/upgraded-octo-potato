739afd0ba9140d42713d9f3c36d56fe892bd0b98
Marco Mangan <marco.mangan@pucrs.br>
2016-09-30 22:36:06 -0300

    Apresentar árvore em formato de hierarquia.

--- bst/src/main/java/br/pucrs/ap3/bst/AVLApp.java
+++ bst/src/main/java/br/pucrs/ap3/bst/AVLApp.java
@@ -6,16 +6,19 @@ package br.pucrs.ap3.bst;
  */
 
 // http://visualgo.net/bst
+//https://docs.oracle.com/javafx/2/ui_controls/tree-view.htm
+//http://docs.oracle.com/javase/tutorial/uiswing/components/tree.html
 public class AVLApp {
 	public static void main(String[] args) {
 		System.out.println("AVL!");
 		AVLBinarySearchTree bst = new AVLBinarySearchTree();
 //		int ns[] = { 30, 20, 10 };
-		int ns[] = { 10, 20, 30 };
-		System.out.println(bst);
+		int ns[] = { 10, 20, 30, 40, 50, 60, 70 };
+		System.out.println(bst.dump());
 		for (int n : ns) {
 			bst.add(n);
-			System.out.println(bst);
+			System.out.println(bst.dump());
+			System.out.println();
 		}
 	}
 }
