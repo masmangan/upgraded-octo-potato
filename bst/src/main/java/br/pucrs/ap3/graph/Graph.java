fc339adc4719661db52eef7f0e76530bd629f9f7
Marco Mangan <marco.mangan@pucrs.br>
2016-10-26 22:45:07 -0200

    TODO: BFS.

--- bst/src/main/java/br/pucrs/ap3/graph/Graph.java
+++ bst/src/main/java/br/pucrs/ap3/graph/Graph.java
@@ -57,4 +57,33 @@ public class Graph {
 		return r;
 	}
 
+	private float d[];
+	private int color[];
+	private int p[];
+
+	public void bfs(int s) {
+		// Linhas 1 até 7
+		d = new float[m.length];
+		for (int i = 0; i < d.length; i++) {
+			d[i] = Float.POSITIVE_INFINITY;
+		}
+		color = new int[m.length]; // WHITE
+		p = new int[m.length]; // NIL
+		d[s] = 0;
+		color[s] = 1; // GRAY
+		p[s] = 0;// NIL
+		// Linha 8
+		List<Integer> Q = new ArrayList<Integer>();
+		// Linha 9
+		Q.add(s);
+		// Linha 10
+		while (!Q.isEmpty()) {
+			Integer u = Q.remove(0);
+			for (Integer v : getNext(u)) {
+				// TODO:
+			}
+			color[u] = 3; // BLACK
+		}
+	}
+
 }
