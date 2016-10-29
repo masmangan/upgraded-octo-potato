98e1be0191f4c4b8ae3a3171f2616af4803bd17f
Marco Mangan <marco.mangan@pucrs.br>
2016-10-28 22:07:52 -0200

    Teste do BFS. #4

--- bst/src/main/java/br/pucrs/ap3/graph/Graph.java
+++ bst/src/main/java/br/pucrs/ap3/graph/Graph.java
@@ -5,6 +5,10 @@ import java.util.List;
 
 public class Graph {
 
+	public static final int WHITE = 0;
+	public static final int GRAY = 1;
+	public static final int BLACK = 2;
+
 	private int[][] m;
 
 	public Graph(int size) {
@@ -43,7 +47,7 @@ public class Graph {
 		m[j][i] = 1;
 	}
 
-	public List<Integer> getNext(int n) {
+	public List<Integer> Adj(int n) {
 		if (n <= 0)
 			throw new IllegalArgumentException();
 		if (n >= m.length)
@@ -57,9 +61,22 @@ public class Graph {
 		return r;
 	}
 
-	private float d[];
+    public float[] getD() {
+        return d;
+    }
+
+    public int[] getColor() {
+        return color;
+    }
+
+    public int[] getP() {
+        return p;
+    }
+
+    private float d[];
 	private int color[];
 	private int p[];
+
 
 	public void bfs(int s) {
 		// Linhas 1 até 7
@@ -70,20 +87,33 @@ public class Graph {
 		color = new int[m.length]; // WHITE
 		p = new int[m.length]; // NIL
 		d[s] = 0;
-		color[s] = 1; // GRAY
+		color[s] = GRAY; // GRAY
 		p[s] = 0;// NIL
 		// Linha 8
 		List<Integer> Q = new ArrayList<Integer>();
 		// Linha 9
-		Q.add(s);
-		// Linha 10
+        ENQUEUE(Q, s);
+        // Linha 10
 		while (!Q.isEmpty()) {
-			Integer u = Q.remove(0);
-			for (Integer v : getNext(u)) {
-				// TODO:
+			Integer u = DEQUEUE(Q);
+			for (Integer v : Adj(u)) {
+				if (color[v] == WHITE) {
+					color[v] = GRAY;
+					d[v] = d[u] + 1;
+					p[v] = u;
+                    ENQUEUE(Q, v);
+				}
 			}
-			color[u] = 3; // BLACK
+			color[u] = BLACK; // BLACK
 		}
 	}
+
+    private Integer DEQUEUE(List<Integer> q) {
+        return q.remove(0);
+    }
+
+    private void ENQUEUE(List<Integer> q, int s) {
+        q.add(s);
+    }
 
 }
